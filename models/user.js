1eb3e341f8e989ba580121483b38bcc264c364a0
bilal <mbilalghz@gmail.com>
2019-03-27 16:51:57 +0700

    register done

--- models/user.js
+++ models/user.js
@@ -1,10 +1,19 @@
 'use strict';
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    name: DataTypes.STRING,
+    fullName: DataTypes.STRING,
     username: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
+    password: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {
+    hooks :{
+      beforeCreate : (user,options) =>{
+        user.password = bcrypt.hashSync(user.password, salt);
+      }
+    }
+  });
   User.associate = function(models) {
     // associations can be defined here
     User.belongsToMany(models.User, {as: 'user', through: models.Friend, foreignKey: 'friendId'})
