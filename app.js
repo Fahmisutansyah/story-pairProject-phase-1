f35b58cc4d4992b76fd4ae50aee46e76ea022427
Fahmisutansyah <fahmisutansyah@gmail.com>
2019-03-27 18:03:39 +0700

    add bcryptCheck add session, login done

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const app = express()
 const port = 8080
 const bodyParser = require('body-parser')
 const model = require('./models')
+const session = require('express-session')
+const auth = require('./middlewares/auth')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'gozali',
+}))
+
 app.set('view engine','ejs')
 
 app.get('/', function (req,res){
@@ -12,13 +18,14 @@ app.get('/', function (req,res){
         include: [{model: model.User, as: "user"}]
     })
     .then(data=>{
-        res.send(data)
+        res.send(req.session)
     })
 })
 
 app.use('/login', require('./routes/loginRoute') )
 
 app.use('/register',require('./routes/registerRoute'))
+
 app.listen(8080, ()=>{
     console.log('server is running')
 })
